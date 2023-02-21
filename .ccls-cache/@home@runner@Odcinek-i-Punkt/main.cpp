ff51b5a2c08cc90bb3d6e1bb20d828b8dd298d2a
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2023-02-21 08:59:53 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Odcinek-i-Punkt/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+struct point{
+  string name;
+  int x;
+  int y;
+
+  print(){
+    cout << "Point: " << name << '(' << x << ',' << y << ')';
+  }
+};
+int main() {
+  
+}
